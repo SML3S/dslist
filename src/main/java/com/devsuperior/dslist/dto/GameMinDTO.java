140d9e2f09b385da3f723e08a4220fb302af2a35
SML3S <30941238+SML3S@users.noreply.github.com>
2024-11-12 23:20:28 -0300

    Projection

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projection.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -15,13 +16,23 @@ public class GameMinDTO {
 	}
 	
 	public GameMinDTO(Game entity) {
-		super();
+		
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+
 
 	public Long getId() {
 		return id;
